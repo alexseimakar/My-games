8a48d58391a2c742b41c7a4f7f6b5765d5f1f84f
Tanckred <makarovaa2001@mail.ru>
2025-01-04 11:07:08 +0300

    Movement, Crosshair, Player Rotation
    improved movement (no diag faster), added crosshair, rotate player to crosshair

--- Roguemetry_Unity/Assets/Scripts/Player_Movement.cs
+++ Roguemetry_Unity/Assets/Scripts/Player_Movement.cs
@@ -6,6 +6,7 @@ public class Player_Movement : MonoBehaviour
 
     float horizontal;
     float vertical;
+    float moveLimit = 0.7f;
 
     public float Movement_speed = 20.0f;
 
@@ -19,10 +20,19 @@ public class Player_Movement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 diference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotateZ = Mathf.Atan2(diference.y, diference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotateZ);
     }
 
     private void FixedUpdate()
     {
+        if (horizontal != 0 && vertical != 0)
+        {
+            horizontal *= moveLimit;
+            vertical *= moveLimit;
+        }
         body.linearVelocity = new Vector2(horizontal * Movement_speed, vertical * Movement_speed);
     }
 }
