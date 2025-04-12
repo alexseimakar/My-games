52c036eb8081a7e29a4ec3bd45880111833822a5
Tanckred <makarovaa2001@mail.ru>
2025-04-12 22:28:25 +0300

    My first game
    Started making by tutorial, hope that will end nice

---
+++ Roguemetry_newconcept/Assets/Scripts/Camera_movement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Camera_movement : MonoBehaviour
+{
+    
+    public Transform target;
+    public Vector3 offset;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + offset;
+    }
+}
---
+++ Roguemetry_newconcept/Assets/Scripts/Player_movement.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotationSpeed;
+    Rigidbody2D rb;
+    Vector2 moveDir;
+
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        InputManagement();
+    }
+    void FixedUpdate()
+    {
+        Move();
+        Rotate();
+    }
+
+    void InputManagement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector2(moveX, moveY).normalized;
+    }
+    
+    void Move()
+    {
+        rb.linearVelocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+    }
+
+    void Rotate()
+    {
+        if (moveDir != Vector2.zero) // Проверяем, движется ли персонаж
+        {
+            // Вычисляем нужный поворот
+            Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, moveDir);
+
+            // Плавно поворачиваем в сторону перемещения
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
+
+
